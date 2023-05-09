8a4575f20f0054e87a9e3c9a180628c04b59c4d5
CTFang's Macbook <andy89923@gmail.com>
2023-05-09 15:27:40 +0800

    add workspace file

---
+++ src/pages/Workspace.tsx
@@ -0,0 +1,9 @@
+function Workspace() {
+  return (
+    <div className="bg-blue-500 p-4 rounded-lg">
+      <span className="text-white">Workspace</span>
+    </div>
+  );
+}
+
+export default Workspace;
