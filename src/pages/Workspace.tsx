1bffa5836ed7eecf608bfa823e3ed57cf6b14cbb
CTFang's Macbook <andy89923@gmail.com>
2023-05-10 15:36:15 +0800

    Workspace Function Section Prototype

--- src/pages/Workspace.tsx
+++ src/pages/Workspace.tsx
@@ -1,44 +1,61 @@
-import { VscArrowLeft, VscSave, VscSaveAs, VscHistory } from "react-icons/vsc";
+import {
+  VscArrowLeft,
+  VscSave,
+  VscSaveAs,
+  VscHistory,
+  VscFolderOpened,
+} from "react-icons/vsc";
 
 // open, save, save as, history button
 const FunctionsSection: React.FC<{ title: string; isFile: boolean }> = (
   props
 ) => {
-  const clickArrow = () => {
-    console.log("click");
+  const clickButton = (e: { currentTarget: { id: any } }) => {
+    console.log(e.currentTarget.id, "click");
+  };
+
+  const MyButton: React.FC<{ id: string; icons: any }> = (props) => {
+    return (
+      <button
+        id={props.id}
+        className="flex w-9/12 flex-row items-center justify-start space-x-2 rounded-2xl px-3 py-1 hover:bg-sky-700"
+        onClick={clickButton}
+      >
+        {props.icons}
+        <p>{props.id}</p>
+      </button>
+    );
   };
 
   const FileOptions: React.FC<{ isFile: boolean }> = (props) => {
     if (props.isFile)
       return (
-        <div className="w-full text-lg py-5 space-y-2 py-3">
-          <button className="w-full flex flex-row space-x-2 items-center pl-5">
-            <VscSave className="scale-125" onClick={clickArrow} />
-            <p>Save</p>
-          </button>
-          <button className="w-full flex flex-row space-x-2 items-center pl-5">
-            <VscSaveAs className="scale-125" onClick={clickArrow} />
-            <p>Save as</p>
-          </button>
-          <button className="w-full flex flex-row space-x-2 items-center pl-5">
-            <VscHistory className="scale-125" onClick={clickArrow} />
-            <p>History</p>
-          </button>
+        <div className="flex w-full flex-col items-center justify-center space-y-4 py-1 text-lg">
+          <MyButton id="Save" icons={<VscSave className="scale-125" />} />
+          <MyButton id="Save as" icons={<VscSaveAs className="scale-125" />} />
+          <MyButton id="History" icons={<VscHistory className="scale-125" />} />
+          <MyButton
+            id="Open"
+            icons={<VscFolderOpened className="scale-125" />}
+          />
         </div>
       );
-    return <></>;
+    return (
+      <div className="flex w-full flex-col items-center justify-center space-y-4 py-1 text-lg">
+        <MyButton id="Open" icons={<VscFolderOpened className="scale-125" />} />
+      </div>
+    );
   };
 
   return (
-    <div className="bg-green-400 w-3/12">
-      <div className="w-full py-3 items-center flex flex-col">
-        <div
-          className="flex flex-row space-x-2 w-9/12 items-center pb-1 \
-          border-transparent border-2 border-b-gray-400"
-        >
-          <VscArrowLeft className="scale-110" onClick={clickArrow} />
-          <p>{props.title}</p>
-        </div>
+    <div className="w-2/12 bg-gray-200">
+      <div className="flex w-full flex-row items-center space-x-2 py-3">
+        <button id="gohome" onClick={clickButton}>
+          <VscArrowLeft className="scale-110" />
+        </button>
+        <p>{props.title}</p>
+      </div>
+      <div className="flex flex-col items-center justify-center">
         <FileOptions isFile={props.isFile} />
       </div>
     </div>
@@ -47,7 +64,7 @@ const FunctionsSection: React.FC<{ title: string; isFile: boolean }> = (
 
 const ProjectSection: React.FC<{}> = () => {
   return (
-    <div className="bg-red-400 w-3/12 py-3  pl-3">
+    <div className="w-3/12 bg-red-400 py-3  pl-3">
       <p>Project</p>
     </div>
   );
@@ -56,7 +73,7 @@ const ProjectSection: React.FC<{}> = () => {
 // File structure Brower
 const FilesSection: React.FC<{ title: string }> = (props) => {
   return (
-    <div className="bg-blue-400 w-3/12 py-3 pl-3">
+    <div className="w-3/12 bg-blue-400 py-3 pl-3">
       <p>{props.title}</p>
     </div>
   );
@@ -64,7 +81,7 @@ const FilesSection: React.FC<{ title: string }> = (props) => {
 
 const PermissionSection: React.FC<{}> = (props) => {
   return (
-    <div className="bg-orange-400 flex-auto py-3 pl-3">
+    <div className="flex-auto bg-orange-400 py-3 pl-3">
       <p>Permission Section</p>
     </div>
   );
@@ -73,7 +90,7 @@ const PermissionSection: React.FC<{}> = (props) => {
 // Main
 const Workspace: React.FC<{}> = (props) => {
   return (
-    <div className="absolute h-full w-full flex divide-x divide-gray-700 text-2xl">
+    <div className="absolute flex h-full w-full divide-x divide-gray-700 text-2xl">
       <FunctionsSection title="Home" isFile={true} />
 
       <ProjectSection />
