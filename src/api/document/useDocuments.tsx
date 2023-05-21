de78e9f8a296f1ffee1bc386fcb28dbbd1746387
y-c-wang <ycwang.cs@gmail.com>
2023-05-21 22:49:54 +0800

    change api format for backend
    (cherry picked from commit 6547ef0eb236e9357d6672857cae5d1e7e45dbe4)

--- src/api/document/useDocuments.tsx
+++ src/api/document/useDocuments.tsx
@@ -1,14 +1,18 @@
-// import axios from 'axios'
+// import axios from "axios";
 import useSWR from "swr";
 import apiConfig from "../apiConfig";
+import { DocumentsRespType } from "../../model/api/document";
 
-// const fetcher = ([url]: [string]) => axios.get(url).then((res) => res.data)
+// const fetcher = ([url]: [string]) => axios.get(url).then((res) => res.data);
 
-const exampleData: string[] = ["File1", "File2", "File3", "File4"];
+const exampleData: DocumentsRespType = {
+  status: "success",
+  documentlist: ["File1", "File2", "File3", "File4"],
+};
 const fetcher = ([url]: [string]) => {
   console.log(url);
   return exampleData;
-}
+};
 const useDocuments = (loadDocuments: boolean) =>
   useSWR(loadDocuments ? [apiConfig.url.document.list()] : null, fetcher);
 
--- src/interfaces/workspace/file.tsx
+++
@@ -1,4 +0,0 @@
-export interface FileType {
-  filename: string;
-  content: string;
-}
