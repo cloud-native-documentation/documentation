eaadd28c8da11abb7c0b8639bc6545c3f228addf
y-c-wang <ycwang.cs@gmail.com>
2023-05-21 14:13:37 +0800

    change zustand store filenames

--- src/store/workspace/index.tsx
+++ src/store/workspace/index.tsx
@@ -1,4 +1,4 @@
-export { default as useFileStore } from "./file";
-export { default as useIsHistoryStore } from "./isHistory";
-export { default as useTabsStore } from "./tabs";
-export { default as useVersionStore } from "./version";
+export { default as useFileStore } from "./useFileStore";
+export { default as useIsHistoryStore } from "./useIsHistoryStore";
+export { default as useTabsStore } from "./useTabsStore";
+export { default as useVersionStore } from "./useVersionStore";
