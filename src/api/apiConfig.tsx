8decc4a3058f22b1469d61dc6da74603c0150aad
y-c-wang <ycwang.cs@gmail.com>
2023-06-05 06:47:17 +0800

    env api url

--- src/api/apiConfig.tsx
+++ src/api/apiConfig.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useAuthStore } from "../store/auth";
 
-const API_URL = "http://127.0.0.1:8000";
+const API_URL = import.meta.env.VITE_API_URL || "http://127.0.0.1:8000";
 
 const apiConfig = {
   url: {
